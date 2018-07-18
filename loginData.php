c8396537bada1685d0e4f62e9330222e737d8c00
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-07-18 22:34:08 +0200

    hasing password

--- loginData.php
+++ loginData.php
@@ -1,22 +1,19 @@
 <?php
 
-
 require 'connection.php';
 
 $email = $_REQUEST['email'];
 $pass = $_REQUEST['pass'];
-
 
 if ($_REQUEST['task'] == "login") {
 
     $sql = "SELECT `email`, `pass` FROM `registracija` WHERE `email` = '$email'";
     $result = $connection->query($sql);
 
-
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             if ($row['email'] == $email) {
-                if ($row['pass'] == $pass) {
+                if (password_verify($pass, $row['pass'])) {
                     $query = "INSERT INTO login (`name`,`pass`) VALUES ('$email', '$pass')";
 
                     $response = @mysqli_query($connection, $query);
@@ -36,5 +33,3 @@ if ($_REQUEST['task'] == "login") {
         }
     }
 }
-
-?>
--- registerData.php
+++ registerData.php
@@ -2,16 +2,15 @@
 
 require 'connection.php';
 
-
 $name = $_REQUEST['name'];
 $surname = $_REQUEST['surname'];
 $email = $_REQUEST['email'];
 $pass = $_REQUEST['pass'];
 
-
+$hash_pass = password_hash($pass, PASSWORD_DEFAULT);
 
 if ($_REQUEST['task'] == "register") {
-    $query = "INSERT INTO registracija (`email`, `name`, `surname`, `pass`) VALUES ('$email','$name', '$surname', '$pass')";
+    $query = "INSERT INTO registracija (`email`, `name`, `surname`, `pass`) VALUES ('$email','$name', '$surname', '$hash_pass')";
 
     $response = @mysqli_query($connection, $query);
     if ($response) {
@@ -20,5 +19,3 @@ if ($_REQUEST['task'] == "register") {
         echo mysqli_error($dbc);
     }
 }
-
-?>
