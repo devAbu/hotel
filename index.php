1d91490b525d3b27962fff449b80571c8b96a06d
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-09-05 18:23:45 +0200

    ...

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ session_start();
     <title>Hotel Online System</title>
     <link rel="icon" type="image/ico" href="images/home.ico" />
     <meta name="author" content="combe">
-    <meta name="keywords" content="Hotel, bosnia, reserve, sarajevo, apartment">
+    <meta name="keywords" content="Hotel, bosnia, reserve, sarajevo, room, app, applicaton">
     <meta name="description" content="Hotel online system for check and reserve a hotel room for your holiday ">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
         crossorigin="anonymous">
